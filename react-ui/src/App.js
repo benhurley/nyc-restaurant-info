ff4c67f03d78c4846c4b055f58f46f3ee7cee6b6
Benjamin Hurley <benjamin.hurley@parivedasolutions.com>
2020-08-03 14:02:07 -0400

    clean up README, app.js, and scripts

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,11 +1,10 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [message, setMessage] = useState(null);
   const [isFetching, setIsFetching] = useState(false);
-  const [url, setUrl] = useState('/api');
+  const [url] = useState('/api');
 
   const fetchData = useCallback(() => {
     fetch(url)
@@ -32,34 +31,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         { process.env.NODE_ENV === 'production' ?
             <p>
-              This is a production build from create-react-app.
+              This is a production build of Food Feels. App coming soon!
             </p>
           : <p>
-              Edit <code>src/App.js</code> and save to reload.
+              This is the developer-mode of Food Feels.
             </p>
         }
-        <p>{'« '}<strong>
-          {isFetching
-            ? 'Fetching message from API'
-            : message}
-        </strong>{' »'}</p>
-        <p><a
-          className="App-link"
-          href="https://github.com/mars/heroku-cra-node"
-        >
-          React + Node deployment on Heroku
-        </a></p>
-        <p><a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a></p>
+        {isFetching
+          ? 'Fetching message from API'
+          : 'Server Status: ' + message
+        }
       </header>
     </div>
   );
