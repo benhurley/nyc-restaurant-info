8a05db3b953b6eb4fd8c3d086c8e2297394cacc2
Benjamin Hurley <benfromtech@gmail.com>
2020-08-04 17:13:51 -0400

    test absolute path in heroku for mongoUtils

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
-var mongoUtil = require('./mongoUtil');
+const mongoUtil = isDev ? require('./mongoUtil') : require('/app/server/mongoUtil.js');
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
