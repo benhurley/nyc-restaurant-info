a817b5f18c7423a7c2c74ef99f26eba88bbddf42
Benjamin Hurley <benfromtech@gmail.com>
2020-08-05 21:38:17 -0400

    random change

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -32,7 +32,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Food Feels - test
+          Food Feels test
         </h1>
         { process.env.NODE_ENV === 'production' ?
             <p>
