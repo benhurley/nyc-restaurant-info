2556fb63fdd1b575f3a72a0faf262e7b5c3881fc
Eric <Natelsoneric@gmail.com>
2020-08-10 17:50:13 -0400

    React router implementation

---
+++ react-ui/src/components/Home-page/Home-page.js
@@ -0,0 +1,32 @@
+import React, { useCallback, useEffect, useState, Fragment } from 'react';
+
+
+export default function HomePage(props) {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+              Food Feels
+            </h1>
+            { process.env.NODE_ENV === 'production' ?
+                <p>
+                  Website coming soon!
+                </p>
+              : <Fragment>
+                  <p>
+                    This is the developer-mode of Food Feels.
+                  </p>
+                  <form method="post" action="/api/restaurants">
+                  <label>Add a Restaurant</label><br />
+                  <input type="text" name="name" placeholder="Name" required /> <br />
+                  <input type="text" name="playlist" placeholder="Music Playlist" required /> <br />
+                  <input type="text" name="lighting" placeholder="Lighting" required /> <br />
+                  <input type="text" name="scent" placeholder="Aromas" required /> <br />
+                  <input type="submit" value="Submit" />
+                  </form>
+              </Fragment>
+            }
+          </header>
+        </div>
+    );
+}
---
+++ react-ui/src/components/Restaurant-info/Restaurant-info.js
@@ -0,0 +1,7 @@
+import React, { useCallback, useEffect, useState, Fragment } from 'react';
+
+function RestaurantInfo(props) {
+    return <h1>Render this</h1>
+}
+
+export default RestaurantInfo;
