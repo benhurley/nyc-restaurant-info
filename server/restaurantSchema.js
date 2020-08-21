38965c8a48e5355e65787bf8808e8a5ee2b103e7
Benjamin Hurley <benfromtech@gmail.com>
2020-08-21 13:18:39 -0400

    clean up schema

--- server/restaurantSchema.js
+++ server/restaurantSchema.js
@@ -1,18 +1,17 @@
 let validator = require('validator')
 
 module.exports = {
-    name: { type: String, required: true},
-    city: { type: String, required: true},
-    state: { type: String, enum: ['NY', 'FL', 'TX'], required: true},
+    name: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, enum: ['NY', 'FL', 'TX'], required: true },
     playlist: { type: String },
     playlistUrl: { type: String, validate: (value) => {
       return validator.isUrl(value)
     }},
-    scent: { type: String},
+    scent: { type: String },
     lights: { type: String, enum: ['dim', 'normal', 'bright'] },
-    items: { type: Array},
+    items: { type: Array },
     referralUrl:  { type: String, validate: (value) => {
       return validator.isUrl(value)
     }}
 };
-
