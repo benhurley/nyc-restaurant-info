8073821b701439d89479dbfd6f115f4ca58dc705
Benjamin Hurley <benfromtech@gmail.com>
2020-08-23 14:55:20 -0400

    remove broken validation

--- server/restaurantSchema.js
+++ server/restaurantSchema.js
@@ -1,16 +1,10 @@
-let validator = require('validator')
-
 module.exports = {
     name: { type: String, required: true },
     city: { type: String, required: true },
     state: { type: String, enum: ['NY', 'FL', 'TX'], required: true },
-    playlistUrl: { type: String, validate: (value) => {
-      return validator.isUrl(value)
-    }},
+    playlistUrl: { type: String },
     scent: { type: String },
     lights: { type: String, enum: ['dim', 'normal', 'bright'] },
     items: { type: Array },
-    referralUrl:  { type: String, validate: (value) => {
-      return validator.isUrl(value)
-    }}
+    referralUrl: { type: String },
 };
