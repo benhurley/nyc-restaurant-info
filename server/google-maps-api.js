f1cb3936f04e7c8bb1588b7ea81513a6876a3356
Eric <Natelsoneric@gmail.com>
2020-09-17 14:05:28 -0500

    stash info away

--- server/google-maps-api.js
+++ server/google-maps-api.js
@@ -1,6 +1,6 @@
-const geoLocationUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-const axios = require('axios');
 const { default: Axios } = require('axios');
+const googleApiKey = process.env.GOOGLE_API_KEY;
+const geoLocationUrl = `https://maps.googleapis.com/maps/api/geocode/json?key=${googleApiKey}&address=`;
 
 async function getCoordinatesFromAddress(address) {
     return Axios.get(`${geoLocationUrl}${address}`);
