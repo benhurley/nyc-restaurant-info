04594533cbd443b1f02bde27e7719ef8a35dbeae
Benjamin Hurley <benfromtech@gmail.com>
2021-08-25 22:01:01 -0400

    update all headers with new tm png

--- react-ui/src/components/Restaurant_Detail/Restaurant_Detail.js
+++ react-ui/src/components/Restaurant_Detail/Restaurant_Detail.js
@@ -342,7 +342,10 @@ export const RestaurantDetail = (props) => {
                         </div>
                     </Link>
                     <Link to={"/"} style={{ textDecoration: 'none', color: "black" }}>
-                        <h1> nyc restaurant info™ </h1>
+                        <h1> 
+                            nyc restaurant info
+                            <img alt="working trademark" className="tm" src={require("../../helpers/tm.png")} />
+                        </h1>                    
                     </Link>
                 </header>
             </div>
