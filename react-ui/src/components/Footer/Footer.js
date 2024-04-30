68b5f12eccda5b426edd3b7d5a7134044d62ee87
Ben Hurley <benjaminhurley@pm.me>
2024-04-30 00:17:38 -0400

    fix: nyc open data link

--- react-ui/src/components/Footer/Footer.js
+++ react-ui/src/components/Footer/Footer.js
@@ -13,7 +13,7 @@ export const Footer = () => {
       </div>
       <div>
         <p>NYC Restaurant Info™ 2020-2024. All rights reserved. Originally created by <a href='https://justben.fyi' target="_blank" rel="noopener noreferrer">Ben</a> and <a target="_blank" rel="noopener noreferrer" href='https://ericnatelson.dev'>Eric </a> during the pandemic to show people what restaurants were still open in NYC.</p>
-        This open-source project shows restaurant inspection data provided by <a href="https://data.cityofnewyork.us/resource/gra9-xbjk.json">NYC Open Data</a>.
+        This open-source project shows restaurant inspection data provided by <a href="https://data.cityofnewyork.us/Health/Restaurant-Grades">NYC Open Data</a>.
         We recommend this website to be used alongside other popular search engines to improve the accuracy of your NYC restaurant search results.
       </div>
     </div>
